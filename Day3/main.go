0043814e52e035f6aba171557983f0cba0aefdb4
Jona Kasten <jona.kasten@siemens.com>
2023-12-04 21:27:15 +0100

    add attempt for day3 part two

--- Day3/main.go
+++ Day3/main.go
@@ -23,6 +23,7 @@ func main() {
 		lineSlice = append(lineSlice, line)
 	}
 
+	gearPairings := map[Coordinates][]int{}
 	sum := 0
 	currentNumber := 0
 	isPartNumber := false
@@ -32,71 +33,120 @@ func main() {
 			sum += currentNumber
 		}
 		isPartNumber = false
+		isGear := false
+		gearCoordinates := Coordinates{}
 		currentNumber = 0
 		for col, c := range line {
-			surroundings := make([]rune, 0)
+			//surroundings := make([]rune, 0)
+			surroundings := map[rune]Coordinates{}
 
 			if unicode.IsDigit(c) {
 				if row > 0 && col > 0 {
 					topLeft := rune(lineSlice[row-1][col-1])
-					surroundings = append(surroundings, topLeft)
+					surroundings[topLeft] = Coordinates{
+						x: row - 1,
+						y: col - 1,
+					}
 				}
 
 				if row > 0 {
 					top := rune(lineSlice[row-1][col])
-					surroundings = append(surroundings, top)
+					surroundings[top] = Coordinates{
+						x: row - 1,
+						y: col,
+					}
 				}
 
 				if col > 0 {
 					left := rune(lineSlice[row][col-1])
-					surroundings = append(surroundings, left)
+					surroundings[left] = Coordinates{
+						x: row,
+						y: col - 1,
+					}
 				}
 
 				if col < len(line)-1 && row > 0 {
 					topRight := rune(lineSlice[row-1][col+1])
-					surroundings = append(surroundings, topRight)
+					surroundings[topRight] = Coordinates{
+						x: row - 1,
+						y: col + 1,
+					}
 				}
 
 				if col < len(line)-1 {
 					right := rune(lineSlice[row][col+1])
-					surroundings = append(surroundings, right)
+					surroundings[right] = Coordinates{
+						x: row,
+						y: col + 1,
+					}
 				}
 
 				if row < len(lineSlice)-1 {
 					bottom := rune(lineSlice[row+1][col])
-					surroundings = append(surroundings, bottom)
+					surroundings[bottom] = Coordinates{
+						x: row + 1,
+						y: col,
+					}
 				}
 
 				if row < len(lineSlice)-1 && col > 0 {
 					bottomLeft := rune(lineSlice[row+1][col-1])
-					surroundings = append(surroundings, bottomLeft)
+					surroundings[bottomLeft] = Coordinates{
+						x: row + 1,
+						y: col - 1,
+					}
 				}
 
 				if row < len(lineSlice)-1 && col < len(line)-1 {
 					bottomRight := rune(lineSlice[row+1][col+1])
-					surroundings = append(surroundings, bottomRight)
+					surroundings[bottomRight] = Coordinates{
+						x: row + 1,
+						y: col + 1,
+					}
 				}
 
 				currentNumber *= 10
 				currentNumber += int(c - '0')
 
-				for _, char := range surroundings {
+				for char, co := range surroundings {
 					if IsSymbol(char) {
 						isPartNumber = true
-						break
+					}
+					if char == '*' {
+						isGear = true
+						gearCoordinates = co
 					}
 				}
 			} else {
 				if isPartNumber {
-					fmt.Println(currentNumber)
 					sum += currentNumber
+				}
+				if isGear {
+					if _, ok := gearPairings[gearCoordinates]; !ok {
+						gearPairings[gearCoordinates] = []int{}
+					}
+					gearPairings[gearCoordinates] = append(gearPairings[gearCoordinates], currentNumber)
 				}
 				currentNumber = 0
 				isPartNumber = false
+				isGear = false
+				gearCoordinates = Coordinates{}
 			}
 
 		}
 	}
+	ratioSum := 0
+	for _, ratioList := range gearPairings {
+		if len(ratioList) > 1 {
+			ratioProd := 1
+			for _, ratio := range ratioList {
+				ratioProd *= ratio
+			}
+			ratioSum += ratioProd
+		}
+	}
+
+	fmt.Println(ratioSum)
 	fmt.Println(sum)
 
 	defer file.Close()
@@ -104,4 +154,9 @@ func main() {
 
 func IsSymbol(c rune) bool {
 	return !unicode.IsDigit(c) && c != '.'
+}
+
+type Coordinates struct {
+	x int
+	y int
 }
