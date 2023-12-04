175cca61a54ea7af8faa257d711f42f9fe14db79
jonakasten <jonakasten@yahoo.de>
2023-12-04 21:44:19 +0100

    add day 4 first part

---
+++ Day4/first_part.py
@@ -0,0 +1,13 @@
+
+with open("input.txt", "r") as cards:
+    total_sum = 0
+    for card in cards:
+        card = card.split(": ")[1:][0]
+        winning_numbers = [int(x) for x in card.split("|")[0].split()]
+        my_numbers = [int(x) for x in card.split("|")[1].split()]
+        current_sum = 1
+        for number in my_numbers:
+            if number in winning_numbers:
+                current_sum *= 2
+        total_sum += int(current_sum/2)
+    print(total_sum)
